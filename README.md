58c5dc03a16b9a760ccc258f39a096c6d6052c88
AthiraSuresh03 <107237033+AthiraSuresh03@users.noreply.github.com>
2022-10-11 11:58:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wmkhce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wmkhce)
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Create a Table builder!</h1>
+      <p>
+        Table should be build from the JSON provided in the file
+        <b> src/dummy.json</b>
+      </p>
+      User should be able to <b>delete</b> a row and<b> hide/view</b> a column.
+      The table should be dynamic and column Name should be 'key' in the json.
+      eg: this table will contain the columns:
+      <b> name, job, eid, age, place, country.</b>
+      <p>
+        <b>
+          Note: Json can have any key value pairs, we don't hardcode the keys{' '}
+        </b>
+      </p>
+      <ul>
+        <li>
+          {' '}
+          The UI should contain a list of the columns and user should be able to
+          toggle it to either show/hide.
+        </li>
+        <li>
+          A table which is populated as per the result of the show/hide
+          configuration.
+        </li>
+      </ul>
+    </div>
+  );
+}
